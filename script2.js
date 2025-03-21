41e797ccd280371566d3c49ae8c0866379cd9c7d
Braian Lucas Escher <braian.escher@gmail.com>
2025-03-21 12:30:28 -0300

    UPDATE- correções

--- script2.js
+++ script2.js
@@ -12,16 +12,16 @@ function verificarSinal(sinal) {
 verificarSinal(-10)
 
 function convertTemp(c) {
-    console.log((c * 9/5) + 32)
-  }
-  convertTemp(27)
+    console.log((c * 9 / 5) + 32)
+}
+convertTemp(27)
 
-function somatorio(i, soma){
+function somatorio(i, soma) {
     while (i < 100) {
         i = (i + 1)
         soma = (soma + i)
         console.log(i)
-console.log("o resultado da soma é ", soma)
+        console.log("o resultado da soma é ", soma)
     }
 
 }
