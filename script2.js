c96dfb13dd1cd04cb3b9da948c39f75ef3843c41
Braian Lucas Escher <braian.escher@gmail.com>
2025-03-21 12:58:29 -0300

    UPDATE- quase lá

--- script2.js
+++ script2.js
@@ -26,3 +26,50 @@ function somatorio(i, soma) {
 
 }
 somatorio(0, 0)
+
+function substituirPalavra(texto) {
+    console.log(texto.replace("azul", "vermelho"))
+}
+substituirPalavra("azul")
+
+function verificarTamanho(texto) {
+    (texto.length > 10) ? console.log("o tamanho é maior que 10") : console.log("o tamanho não é maior que 10")
+}
+verificarTamanho("adoramos passear pelo bairro")
+
+let frutas = ["banana", "pequi", "kiwi", "abacaxi", "jambu"]
+console.log(frutas[2])
+
+function saudacao(nome, saudacao) {
+    (saudacao === "") ? console.log('Bem-vindo(a) ' + nome) : console.log(saudacao + " " + nome)
+}
+saudacao("braian", "Bem-vindo(a)")
+
+function verificarAprovacao(nota) {
+    (nota >= 6) ? console.log('aprovado') : console.log('reprovado')
+}
+verificarAprovacao(5)
+
+for (let i = 0; i <= 30; i++) {
+    if (i % 2 === 0) {
+        console.log(i)
+    }
+}
+
+function contarVogais(texto) {
+    console.log(texto.indexOf("a", "e", "i", "o", "u") + 1)
+}
+contarVogais("abestado")
+
+function somarArray(x) {
+    let y;
+    for (let i = 0; i < x.length; i++) {
+        y += x[i];
+    }
+    console.log(y);
+}
+
+function dobrarNumero(n) {
+    return n * 2;
+}
+
